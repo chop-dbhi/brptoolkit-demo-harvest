fbdab0e95c66988b060424abc52e04788b60d79f
r3m0 williams <r3m0@noflesh.com>
2016-04-18 08:00:51 -0400

    removed server-specific start script

--- start_application.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --restart always --link postgres --link nginx --link memcached --name brp_demo_harvest --env-file /opt/apps/brptoolkit-demo-harvest/production.env -d demo-harvest
